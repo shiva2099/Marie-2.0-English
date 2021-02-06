3cd7276c11d4c33de1a66e4fa05ee7eb4b65544f
shiva2099 <76653639+shiva2099@users.noreply.github.com>
2021-02-06 15:30:14 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,18 +18,10 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
+hoi {}, my name is {}! Gotha unnaku use panna rights illa da badu.i will block u
 
-im a group manager bot maintained by  [this person](tg://user?id={}).
+im a assistant for ma thala dukelover @tamilrockersvpn admins
 
-My future updates will be put into This Channel - @MarieChechi & My Support Group @InFoTelGroup.
-
-This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/TGExplore/Marie-2.0-English),
-you can create clone same like me..
-
-For more commands click /help...
-
-**Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
 
 """
 
